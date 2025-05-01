b3ddf560c7eb7b8fddec3bbd99a58e1d40b1fa28
yunus.tzr <yunus.tzr>
2025-05-01 19:57:49 +0300

    feature

---
+++ AuthenticationApi/Domain/Models/ENTITY/Notification.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationApi.Domain.Models.ENTITY
+{
+    public class Notification
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid? UserId { get; set; }  // null ise tüm kullanıcılara bildirim
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public bool IsRead { get; set; } = false;
+        public bool IsSent { get; set; } = false;        // Gönderildi mi?
+        public bool ShouldDisplay { get; set; } = true; // Oluşturulduğunda gösterim durumu
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DisplayAt { get; set; }        // Gösterim zamanı, null ise hemen
+        public DateTime? SentAt { get; set; }           // Gönderim zamanı
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+    }
+
+}
